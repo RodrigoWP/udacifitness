291398b10e4a4e28fabbdcac8b56d0ccb153f9b9
Rodrigo Wippel <rodrigow1992@gmail.com>
2017-12-03 21:23:10 -0200

    continuando estilização

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { View } from 'react-native'
-import AddEntry from './components/add-entry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/history'
 
 export default () => (
   <Provider store={createStore(reducer)}>
     <View style={{flex: 1}}>
-      <AddEntry />
+      <History />
     </View>
   </Provider>
 )
